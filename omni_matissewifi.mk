dd88ae7f4217765d270fd607aac28a5b693ee04b
matteo0026 <marmotta200026@gmail.com>
2019-11-04 21:10:22 +0100

    Now sources are good. Compile kernel and add useful stuff.

--- omni_matissewifi.mk
+++ omni_matissewifi.mk
@@ -1,4 +1,16 @@
+# Release name
+PRODUCT_RELEASE_NAME := matissewifi
+
+# Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Time Zone data for Recovery
+PRODUCT_COPY_FILES += \
+    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+
 PRODUCT_DEVICE := matissewifi
 PRODUCT_NAME := omni_matissewifi
 PRODUCT_BRAND := samsung
